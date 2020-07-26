762df7ba5bec030b5780709cb1d0c278cf9ad1b0
Mitchell Case <mectx02@icloud.com>
2020-07-25 20:26:15 -0500

    Update to chat client
    Addad a GUI interface for the Client so that changing ip addresses is easier. GUI only allows *.*.*.* addresses or else defaults to localhost.

--- Basic Chat Clients/ChattableClient.java
+++ Basic Chat Clients/ChattableClient.java
@@ -3,12 +3,22 @@ package alwaysTrue;
 import java.io.*;
 import java.util.*;
 import java.net.*;
+import javax.swing.*;
 
 public class ChattableClient {
 
     public static void main(String[] args) throws IOException {
 
-        Socket socket = new Socket("localhost", 5029);
+        //fancy way to set the ip address. Just shows an input dialog asking for an IP address to connect to.
+        String ipAddress = JOptionPane.showInputDialog(null, "Please enter an IP address that you want to connect to.\n" +
+                "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tLeave blank to default to localhost."); //Formatting is wonderful kids...
+        Socket socket;
+
+        if (ipAddress.matches("[0-9]{3}.[0-9]{3}.[0-9]{3}.[0-9]{3}"))
+            socket = new Socket(ipAddress, 5029);
+        else
+            socket = new Socket("localhost", 5029);
+
         Scanner keyboard = new Scanner(System.in);
 
         DataInputStream clientInput = new DataInputStream(socket.getInputStream());
